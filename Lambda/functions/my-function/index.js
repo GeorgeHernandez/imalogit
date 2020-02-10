ebfddd6fe1072a3a7ed3e4d9d2cab5caeb23842e
George Hernandez <georgelhernandez@gmail.com>
2020-02-10 16:20:01 -0600

    JWT validated. Needs to return less info.

--- Lambda/functions/my-function/index.js
+++ Lambda/functions/my-function/index.js
@@ -3,18 +3,9 @@ const zession = require('./zorg/zession.js');
 exports.handler = async (event) => {
     const authorization = event.headers.Authorization; // After authenticated by Cognito.
     const token = authorization.slice(7); // Remove the 'Bearer ' prefix.
-
-    // let session = new Zession(token);
-    // let body = {};
     let body = await zession.validateToken(token);
-    body.myAsync = await zession.myAsync('ma');
-    body.myNoAsync = zession.myNoAsync('mna');
-    body.myProperty = zession.myProperty;
 
-    // Now do whatever you want with the verified claims!
-    body.email = body.goodSignature.email;
-    body.sub = body.goodSignature.sub; // Subject, i.e. the UUID for the user in the Cognito user pool
-    // console.log("event: ", util.inspect(event, { showHidden: false, depth: null })); // To see input in CloudWatch
+    // A real function would check body.isValid, then do other stuff.
 
     const response = {
         statusCode: 200,
--- Lambda/functions/my-function/zorg/zession.js
+++ Lambda/functions/my-function/zorg/zession.js
@@ -8,15 +8,21 @@ const jtp = require('jwk-to-pem');
 const jwt = require('jsonwebtoken');
 const request = require('request');
 
+const body = {isValid: true};
+
 async function validateToken(token) {
     // Verifying a JSON Web Token (https://docs.aws.amazon.com/en_pv/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html)
 
-    // TODO: Change so that each step must be true before proceeding and that all steps must be true.
+    // Each step must be true before proceeding and  all steps must be true.
 
     // Step 1: Confirm the Structure of the JWT
     const JWS_REGEX = /^[A-z0-9\-_=]+?\.[A-z0-9\-_=]+?\.[A-z0-9\-_=]+?$/; // E.g. aB1-_=.cD2-_=.eF3-_=
-    let body = {};
-    body.goodStructure = JWS_REGEX.test(token);
+    if (JWS_REGEX.test(token)) {
+        body.goodStructure = true;
+    } else {
+        body.goodStructure = false;
+        body.isValid = false;
+    }
 
     // Step 2: Validate the JWT Signature
     // Step 2.1: Decode the local Key ID (kid) from the token
@@ -41,42 +47,60 @@ async function validateToken(token) {
         body.keyIndex = 1;
     } else {
         body.goodKid = false;
+        body.isValid = false;
     }
 
     // Step 2.3: Use key to verify the signature.
     var pem = jtp(publicJwks.keys[body.keyIndex]);
     body.pem = pem;
-    const claims = jwt.verify(token, pem); // TODO: Handle err.
-    body.goodSignature = claims;
+    try {
+        var claims = jwt.verify(token, pem); // TODO: Handle err.
+        body.goodSignature = claims;
+    } catch(error) {
+        body.goodSignature = error;
+        body.isValid = false;
+    }
 
     // Step 3: Verify the Claims
     // Step 3.1: Verify that the token is not expired.
     const expirationTime = claims.exp; // seconds since 1970-01-01 0:0:0Z
     const currentTime = Math.floor(Date.now() / 1000);
-    body.goodTime = (currentTime < expirationTime);
+    if (currentTime < expirationTime) {
+        body.goodTime = true;
+    } else {
+        body.goodTime = false;
+        body.isValid = false;
+    }
 
     // Step 3.2: Verify that the aud claim matches the app client ID in the Cognito user pool.
     const appClientId = process.env.COGNITO_CLIENT_ID;
-    body.goodAppId = (claims.aud === appClientId);
+    if (claims.aud === appClientId) {
+        body.goodAppId = true;
+    } else {
+        body.goodAppId = false;
+        body.isValid = false;
+    }
 
     // Step 3.3: Verify that the iss claim matches the Cognito user pool.
-    body.goodIssuer = (claims.iss === userPoolUrl);
+    if (claims.iss === userPoolUrl) {
+        body.goodIssuer = true;
+    } else {
+        body.goodIssuer = false;
+        body.isValid = false;
+    }
 
     // Step 3.4: Verify the token_use claim.
     // In our case it is `id` instead of `access`
     body.goodUse = (claims.token_use === 'id');
+    if (claims.token_use === 'id') {
+        body.goodUse = true;
+    } else {
+        body.goodUse = false;
+        body.isValid = false;
+    }
+
+    // TODO: Call refreshToken()
 
     return body;
 }
-
 exports.validateToken = validateToken;
-exports.myProperty = 'bar';
-
-async function myAsync(str) {
-    return 'an async result';
-}
-exports.myAsync = myAsync;
-
-exports.myNoAsync = (s) => 'a non-async result';
-
-// TODO: refreshToken()
