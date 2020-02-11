8462f176ee6ebd7c26031dd44b937e0039fd6a9e
George Hernandez <georgelhernandez@gmail.com>
2020-02-11 10:27:22 -0600

    Exception handling done for now.

--- Lambda/functions/my-function/index.js
+++ Lambda/functions/my-function/index.js
@@ -1,32 +1,33 @@
 const zession = require('./zorg/zession.js');
 
 exports.handler = async (event) => {
-    const authorization = event.headers.Authorization; // After authenticated by Cognito.
-    const token = authorization.slice(7); // Remove the 'Bearer ' prefix.
-    let session;
-    let data = {}; // Fill with session meta + whatever the real function should.
-    data.session = {};
-    try {
-        session = await zession.validateToken(token);
-        if (session.isValid) {
-            data.session.isValid = true;
-            data.session.claims = session.goodSignature;
-        } else {
-            data.session.isValid = false;
-            // Add other info as needed.
-        }
-        // A real function would do the above session check, but then do other stuff.
-    } catch(e) {
-        data.session.isValid = false;
+  const authorization = event.headers.Authorization; // After authenticated by Cognito.
+  const token = authorization.slice(7); // Remove the 'Bearer ' prefix.
+  let session;
+  let data = {}; // Fill with session meta + whatever the real function should.
+  data.session = {};
+  try {
+    session = await zession.validateToken(token);
+    if (session.isValid) {
+      data.session.isValid = true;
+      data.session.claims = session.goodSignature;
+    } else {
+      data.session.isValid = false;
+      // Add other info as needed.
     }
+    // A real function would do the above session check, but then do other stuff.
+  } catch(e) {
+    data.session.error = e;
+    data.session.isValid = false;
+  }
 
-    const response = {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': 'https://imalogit.com',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    };
-    return response;
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://imalogit.com',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+  return response;
 };
--- Lambda/functions/my-function/zorg/zession.js
+++ Lambda/functions/my-function/zorg/zession.js
@@ -10,18 +10,18 @@ const request = require('request');
 
 const body = {isValid: true};
 
-async function validateToken(token) {
-    // Verifying a JSON Web Token (https://docs.aws.amazon.com/en_pv/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html)
+async function validateToken (token) {
+  // Verifying a JSON Web Token (https://docs.aws.amazon.com/en_pv/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html)
+  // Each step must be true before proceeding and all steps must be true.
 
-    // Each step must be true before proceeding and  all steps must be true.
-
+  try {
     // Step 1: Confirm the Structure of the JWT
     const JWS_REGEX = /^[A-z0-9\-_=]+?\.[A-z0-9\-_=]+?\.[A-z0-9\-_=]+?$/; // E.g. aB1-_=.cD2-_=.eF3-_=
     if (JWS_REGEX.test(token)) {
-        body.goodStructure = true;
+      body.goodStructure = true;
     } else {
-        body.goodStructure = false;
-        body.isValid = false;
+      body.goodStructure = false;
+      body.isValid = false;
     }
 
     // Step 2: Validate the JWT Signature
@@ -40,25 +40,25 @@ async function validateToken(token) {
     const publicJwks = await publicJwksResponse.json();
     body.publicJwks = publicJwks;
     if (kid === publicJwks.keys[0].kid) {
-        body.goodKid = true;
-        body.keyIndex = 0;
+      body.goodKid = true;
+      body.keyIndex = 0;
     } else if (kid === publicJwks.keys[1].kid) {
-        body.goodKid = true;
-        body.keyIndex = 1;
+      body.goodKid = true;
+      body.keyIndex = 1;
     } else {
-        body.goodKid = false;
-        body.isValid = false;
+      body.goodKid = false;
+      body.isValid = false;
     }
 
     // Step 2.3: Use key to verify the signature.
-    var pem = jtp(publicJwks.keys[body.keyIndex]);
+    const pem = jtp(publicJwks.keys[body.keyIndex]);
     body.pem = pem;
-    try {
-        var claims = jwt.verify(token, pem); // TODO: Handle err.
-        body.goodSignature = claims;
-    } catch(error) {
-        body.goodSignature = error;
-        body.isValid = false;
+    const claims = jwt.verify(token, pem);
+    if (claims)  {
+      body.goodSignature = claims;
+    } else {
+      body.goodSignature = {};
+      body.isValid = false;
     }
 
     // Step 3: Verify the Claims
@@ -66,41 +66,45 @@ async function validateToken(token) {
     const expirationTime = claims.exp; // seconds since 1970-01-01 0:0:0Z
     const currentTime = Math.floor(Date.now() / 1000);
     if (currentTime < expirationTime) {
-        body.goodTime = true;
+      body.goodTime = true;
     } else {
-        body.goodTime = false;
-        body.isValid = false;
+      body.goodTime = false;
+      body.isValid = false;
     }
 
     // Step 3.2: Verify that the aud claim matches the app client ID in the Cognito user pool.
     const appClientId = process.env.COGNITO_CLIENT_ID;
     if (claims.aud === appClientId) {
-        body.goodAppId = true;
+      body.goodAppId = true;
     } else {
-        body.goodAppId = false;
-        body.isValid = false;
+      body.goodAppId = false;
+      body.isValid = false;
     }
 
     // Step 3.3: Verify that the iss claim matches the Cognito user pool.
     if (claims.iss === userPoolUrl) {
-        body.goodIssuer = true;
+      body.goodIssuer = true;
     } else {
-        body.goodIssuer = false;
-        body.isValid = false;
+      body.goodIssuer = false;
+      body.isValid = false;
     }
 
     // Step 3.4: Verify the token_use claim.
     // In our case it is `id` instead of `access`
     body.goodUse = (claims.token_use === 'id');
     if (claims.token_use === 'id') {
-        body.goodUse = true;
+      body.goodUse = true;
     } else {
-        body.goodUse = false;
-        body.isValid = false;
+      body.goodUse = false;
+      body.isValid = false;
     }
 
     // TODO: Call refreshToken()
+  } catch(e) {
+    body.error = e;
+    body.isValid = false;
+  }
 
-    return body;
+  return body;
 }
 exports.validateToken = validateToken;
