b8bf89868c47372fa563c1bb9954cad6a2e0aea8
George Hernandez <georgelhernandez@gmail.com>
2020-02-13 11:26:32 -0600

    Applied JavaScript Standard styling.

--- Lambda/functions/my-function/index.js
+++ Lambda/functions/my-function/index.js
@@ -1,39 +1,39 @@
-const zession = require('./zorg/zession.js');
+const zession = require('./zorg/zession.js')
 
 exports.handler = async (event) => {
-  const authorization = event.headers.Authorization; // Authenticated by Cognito.
-  const token = authorization.slice(7); // Remove the 'Bearer ' prefix.
-  const bearerType = (token.length > 40) ? 'id_token' : 'code';
+  const authorization = event.headers.Authorization // Authenticated by Cognito.
+  const token = authorization.slice(7) // Remove the 'Bearer ' prefix.
+  const bearerType = (token.length > 40) ? 'id_token' : 'code'
 
-  let session;
-  let data = {}; // Fill with session meta + whatever the real function should.
-  data.session = {};
-  data.session.bearerType = bearerType;
+  let session
+  const data = {} // Fill with session meta + whatever the real function should.
+  data.session = {}
+  data.session.bearerType = bearerType
 
   if (bearerType === 'id_token') {
     try {
-      session = await zession.validateToken(token);
+      session = await zession.validateToken(token)
       if (session.isValid) {
-        data.session.isValid = true;
-        data.session.claims = session.goodSignature;
+        data.session.isValid = true
+        data.session.claims = session.goodSignature
       } else {
-        data.session.isValid = false;
-        throw session.error;
+        data.session.isValid = false
+        throw session.error
       }
       // A real function would do the above session check, but then do other stuff.
-      data.foo = 'bar';
-    } catch(e) {
-      data.session.error = e;
-      data.session.isValid = false;
+      data.foo = 'bar'
+    } catch (e) {
+      data.session.error = e
+      data.session.isValid = false
     }
   } else if (bearerType === 'code') {
-    data.session.isValid = true;
-    data.session.claims = {};
-    data.session.claims.email = token;
+    data.session.isValid = true
+    data.session.claims = {}
+    data.session.claims.email = token
   }
 
-  return makeResponse(data);
-};
+  return makeResponse(data)
+}
 
 function makeResponse (data) {
   const response = {
@@ -43,6 +43,6 @@ function makeResponse (data) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
-  };
-  return response;
+  }
+  return response
 }
---
+++ app/index.js
@@ -0,0 +1,49 @@
+// The user should have signed in with Cognito.
+const hashParams = new URLSearchParams(window.location.hash.substr(1))
+const idToken = hashParams.get('id_token')
+// console.log('id_token: ' + id_token);
+
+const accessToken = hashParams.get('access_token')
+// console.log('access_token: ' + access_token);
+
+const queryParams = new URLSearchParams(window.location.search)
+const code = queryParams.get('code')
+// console.log('code: ' + code);
+
+// Set up:
+const api = 'https://api.imalogit.com/main/dev/'
+const origin = 'https://imalogit.com'
+const bearerType = 'id_token' // set to either id_token, access_token, or code
+let bearerValue
+if (bearerType === 'id_token') {
+  bearerValue = idToken
+} else if (bearerType === 'access_token') {
+  bearerValue = accessToken
+} else if (bearerType === 'code') {
+  bearerValue = code
+}
+const authorization = 'Bearer ' + bearerValue
+
+async function asyncIt (api, origin, authorization) {
+  try {
+    const response = await window.fetch(api, {
+      headers: {
+        Origin: origin,
+        Authorization: authorization
+      },
+      method: 'GET'
+    })
+
+    const data = await response.json()
+    console.log('data: ', data)
+    if (data.session.isValid) {
+      document.getElementById('asyncAnswer').innerHTML = '<mark>' + data.session.claims.email + '</mark> has signed in.'
+    } else {
+      throw JSON.stringify(data)
+    }
+  } catch (e) {
+    console.log('Error: ', e)
+    document.getElementById('asyncAnswer').innerHTML = 'Sorry, there was an error.'
+  }
+};
+asyncIt(api, origin, authorization)
