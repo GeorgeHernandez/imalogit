23e4151520c73c7fa37631082091eaa7ac0f81ca
George Hernandez <georgelhernandez@gmail.com>
2020-03-12 16:08:18 -0500

    Starting logGet.

---
+++ Lambda/functions/logGet.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://imalogit.com',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify('Hello world!')
+  }
+  return response
+}
