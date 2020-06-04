692f747fd93ec220dc2138d383094015245dfbe3
George Hernandez <georgelhernandez@gmail.com>
2020-06-04 12:09:40 -0500

    Updated README

--- Lambda/functions/logGet/index.js
+++ Lambda/functions/logGet/index.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk')
 const sside = require('/opt/nodejs/my/sside.js')
 const ddb = new AWS.DynamoDB()
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const authorization = event.headers.Authorization // Authenticated by Cognito.
   const token = authorization.slice(7) // Remove the 'Bearer ' prefix.
   const bearerType = (token.length > 40) ? 'id_token' : 'code'
@@ -14,6 +14,14 @@ exports.handler = async (event) => {
   results.dataCount = 0 // Usually a count of the objects in the data
   results.data = [] // Usually an array of just key-value objects. E.g. [{name: 'Abe', age: 3}, {name: ' Boo', age: 4}]
 
+  // DEV ONLY:
+  // return makeResponse(results)
+
+  if (bearerType !== 'id_token') {
+    results.message = 'Invalid token'
+    return makeResponse(results)
+  }
+
   const session = await sside.validateToken(token).catch(err => {
     results.message = err.message
     return makeResponse(results)
@@ -22,28 +30,48 @@ exports.handler = async (event) => {
   // A real function would do the above session check, but then do other stuff.
   // For now I'll stick in random stuff
 
-  if (bearerType === 'id_token') {
-    results.success = true
-    results.message = 'Hi from Lambda'
-    results.dataCount = 1
+  results.success = true
+  results.message = 'Hi from ' + context.functionName + '()'
+  results.dataCount = 1
 
-    // DynamoDB data:
-    const params = {
-      TableName: 'imalogit',
-      KeyConditionExpression: 'parent = :v1',
-      ExpressionAttributeValues: { ':v1': { S: 'u.' + session.claims.sub } }
-    }
-    // console.log(params)
-    results.data[0] = params
-    return makeResponse(results)
-
-    // ddb.query(params, function (err, data) {
-    //   if (err) return makeResponse(err, err.stack)
-    //   else return makeResponse(data)
-    // })
-  } else if (bearerType === 'code') {
-    results.message = 'We do not take code'
+  // DynamoDB data:
+  const params = {
+    TableName: 'imalogit',
+    KeyConditionExpression: 'parent = :v1',
+    ExpressionAttributeValues: { ':v1': { S: 'u.' + session.claims.sub } }
   }
+  // console.log(params)
+  // results.data[0] = params
+  // // return results
+  // return makeResponse(results)
+
+  const myPromise = ddb.query(params).promise()
+  // return myPromise // Works
+  // return myPromise.then(data => data).catch(err => err) // Works
+  const answer = myPromise.then(data => data).catch(err => err) // Works
+  return answer // Works
+  // const answerString = JSON.stringify(answer)
+  // return answerString //  "{}"?!
+  // return typeof answer // object
+  // return makeResponse(answer) // body is "{}"?!
+  // const response = makeResponse(answer)
+  // return typeof response // object
+  // return response // body is "{}"?!
+  // return await makeResponse(answer) // body is "{}"?!
+  // return JSON.stringify(answer) // response is "{}"?!
+  // return wrapResults(answer) // body is "{}"?!
+  // return await wrapResults(answer) // body is "{}"?!
+
+  // const response = {
+  //   statusCode: 200,
+  //   headers: {
+  //     'Access-Control-Allow-Origin': 'https://imalogit.com',
+  //     'foo': 'bar',
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: answer
+  // }
+  // return response
 }
 
 function makeResponse (results) {
@@ -56,4 +84,20 @@ function makeResponse (results) {
     body: JSON.stringify(results)
   }
   return response
+}
+
+async function wrapResults (results) {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://imalogit.com',
+      'Content-Type': 'application/json'
+    },
+    body: await asyncStringify(results)
+  }
+  return response
+}
+
+async function asyncStringify (value) {
+  return JSON.stringify(value)
 }
--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Log stuff quickly.
 
-The goal of this app is to allow users to quickly access logs, log entries, and see simple run-time charts. For more, see our [About Us](https://imalogit.com/about.html).
+The goal of this app is to allow users to quickly access logs, log entries, and see simple run-time charts for these logs. For more, see our [About Us](https://imalogit.com/about.html).
 
-This code repository contains front-end code for the site, except the `Labmda` folder which has back-end code.
+Summary notes on the repo content
+
+- `/`. The static site content plus some other stuff like `.gitignore` and the `package.json` used by `cside`.
+- `/app/`. The dynamic site content that interacts with the backend AWS services.
+    - `/app/src/`. The client-side source code that is "compiled" by Browserify to  `/app/`.
+- `/Lambda/`. Backend code for AWS Lambda functions and layers.
+    - `/Lambda/functions/`. My AWS Lambda functions, usuall called by the app vie AWS API Gateway.
+    - `/Lambda/layers/uno/`. My `sside` Node.js for the Lambda functions to use.
+- `/my/`. My `cside` Node.js for the site's app to interact with backend AWS services.
+    - `/my/out/`. Documentation for `cside` that I periodically create with JSDoc.
